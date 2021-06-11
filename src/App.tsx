e84765acda65c40543c6e703727322b98caead09
miyazi777 <m71203@gmail.com>
2021-06-11 23:50:37 +0900

    useReducerサンプル

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+
+type StateType = {
+  count: number
+}
+
+type Actions = {
+  type: 'increment' | 'decrement' | 'plus'
+  payload?: number
+}
 
 function App() {
+  // reducerを定義
+  const countReducer = (state: StateType, action: Actions): StateType => {
+    if (action.type === 'increment') {
+      return { count: state.count + 1 }
+    }
+    if (action.type === 'decrement') {
+      return { count: state.count - 1 }
+    }
+    if (action.type === 'plus') {
+      if (action.payload === undefined) return state;
+      return { count: state.count + action.payload }
+    }
+    return state;
+  }
+
+  // useReducerの第一引数でreducerを、第二引数で初期値を設定
+  const [state, dispatch] = useReducer(countReducer, { count: 0 })
+
+  // dispatchでactionをcallする
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>use reducer sample</h2>
+      <div>count: {state.count}</div>
+      <button onClick={() => dispatch({ type: 'increment'}) }>plus</button>
+      <button onClick={() => dispatch({ type: 'decrement' }) }>minus</button>
+      <button onClick={() => dispatch({ type: 'plus', payload: 3 }) }>plus 3</button>
     </div>
   );
 }
